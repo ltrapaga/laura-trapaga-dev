f4f07de2c3142008aa6cc2bf828678b8e799ca5a
Laura Trapaga <trapaga9@gmail.com>
2023-04-03 17:24:21 -0400

    Create NavBar.js / Add NavBar component to App.js     Create font folder - add custom fonts     Create img folder - add navbar .svg icons     NavBar.js         Import modules, hooks, and images         Create `NavBar` function         Add useState hooks             `activeLink`             `scrolled`         Add useEffect hook             `onScroll`         Add NavBar JSX     Style Navbar         Bootstrap in `NavBar` JSX         CSS in App.css

---
+++ src/components/NavBar.js
@@ -0,0 +1,97 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import stackOverflowIcon from "../assets/img/stackOverflowIcon.svg";
+import linkedInIcon from "../assets/img/linkedInIcon.svg";
+import githubIcon from "../assets/img/githubIcon.svg";
+import { HashLink } from "react-router-hash-link";
+import { BrowserRouter as Router } from "react-router-dom";
+
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      //* Returns true if the scrollbars are visible; otherwise, returns false
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+            {/* TODO: <img src={logo} alt="Logo" />*/}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-media-icon">
+                <a href="https://stackoverflow.com/users/21559321/987ltrap?tab=profile">
+                  <img src={stackOverflowIcon} alt="" />
+                </a>
+                <a href="https://www.linkedin.com/in/laura-trapaga-285852262/">
+                  <img src={linkedInIcon} alt="" />
+                </a>
+                <a href="https://github.com/ltrapaga?tab=repositories">
+                  <img src={githubIcon} alt="" />
+                </a>
+              </div>
+              <HashLink to="#connect">
+                <button className="vvd">
+                  <span>Connect With Me</span>
+                </button>
+              </HashLink>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  );
+};
