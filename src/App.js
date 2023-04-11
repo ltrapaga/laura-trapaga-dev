e44023703cf7d56f755bb103ae7596e1ffc7d212
Laura Trapaga <trapaga9@gmail.com>
2023-04-11 15:33:52 -0400

    Add Contact.js and favicon

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
 import { Home } from "./components/Home";
 import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Home />
       <Projects />
+      <Contact />
     </div>
   );
 }
